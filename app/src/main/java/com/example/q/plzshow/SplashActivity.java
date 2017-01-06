10046f5a16543fcaf42c8b37ca640a2ab980fbc8
alphahoon <alphahoon@kaist.ac.kr>
2017-01-06 21:00:54 +0900

    tab B, C skeleton code generated

---
+++ app/src/main/java/com/example/q/plzshow/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.q.plzshow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.q.plzshow.Fragment.ATabFragment;
+import com.example.q.plzshow.Fragment.BTabFragment;
+import com.example.q.plzshow.Fragment.CTabFragment;
+import com.facebook.login.LoginManager;
+
+public class SplashActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+}
