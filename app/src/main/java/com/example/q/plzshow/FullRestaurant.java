0c3108cb12f6da003c8ad2e71d3581b23e26b87e
YUN MIN KYU <ymk121100@gmail.com>
2017-01-06 20:58:21 +0900

    init

--- app/src/main/java/com/example/q/plzshow/FullRestaurant.java
+++ app/src/main/java/com/example/q/plzshow/FullRestaurant.java
@@ -1,11 +1,19 @@
 package com.example.q.plzshow;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import static android.graphics.Color.*;
 
 /**
  * Created by q on 2017-01-06.
@@ -18,7 +26,30 @@ public class FullRestaurant extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.restaurant_full);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("류니끄");
+        toolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
+
+
+        final Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
+        upArrow.setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_ATOP);
+        toolbar.setNavigationIcon(upArrow);
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 finish();
+             }
+         });
+
+
+
         Intent intent = getIntent();
         String res_id = intent.getExtras().getString("res_id");
         Log.d("res_id", res_id);
