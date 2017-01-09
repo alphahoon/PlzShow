dfce69fa0fffa53b13cc0cdc3c505c4d3769237b
YUN MIN KYU <ymk121100@gmail.com>
2017-01-09 21:09:08 +0900

    A,B,C Merged Need to work on Full_restaurant

--- app/src/main/java/com/example/q/plzshow/FullRestaurant.java
+++ app/src/main/java/com/example/q/plzshow/FullRestaurant.java
@@ -8,7 +8,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.icu.util.Calendar;
+import android.location.Address;
+import android.location.Geocoder;
 import android.media.Image;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -17,17 +21,21 @@ import android.os.Handler;
 import android.preference.DialogPreference;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroupOverlay;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.q.plzshow.adapter.photoAdapter;
@@ -37,24 +45,40 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import com.example.q.plzshow.naver.NMapPOIflagType;
+import com.example.q.plzshow.naver.NMapViewerResourceProvider;
 import com.nhn.android.maps.NMapActivity;
+import com.nhn.android.maps.NMapController;
+import com.nhn.android.maps.NMapOverlay;
+import com.nhn.android.maps.NMapOverlayItem;
 import com.nhn.android.maps.NMapView;
+import com.nhn.android.maps.maplib.NGeoPoint;
+import com.nhn.android.maps.overlay.NMapPOIdata;
+import com.nhn.android.maps.overlay.NMapPOIitem;
+import com.nhn.android.mapviewer.overlay.NMapCalloutOverlay;
+import com.nhn.android.mapviewer.overlay.NMapOverlayManager;
+import com.nhn.android.mapviewer.overlay.NMapOverlayManager.OnCalloutOverlayListener;
+import com.nhn.android.mapviewer.overlay.NMapPOIdataOverlay;
+import com.nhn.android.mapviewer.overlay.NMapResourceProvider;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by q on 2017-01-06.
  */
 
-public class FullRestaurant extends AppCompatActivity {
+public class FullRestaurant extends NMapActivity {
 
-    private NMapView mMapView;// 지도 화면 View
     private final String CLIENT_ID = "a3h1sgm1T4A099FNlI1C";// 애플리케이션 클라이언트 아이디 값
 
     @Override
@@ -158,61 +182,55 @@ public class FullRestaurant extends AppCompatActivity {
                 }
             });
 
+            //네이버 지도 API
+            //네이버 지도 생성
+            NMapView mapView = (NMapView) findViewById(R.id.mapView);
+            mapView.setBuiltInZoomControls(true, null);
+            mapView.setClientId(CLIENT_ID);
+            mapView.setClickable(true);
+            mapView.setFocusable(true);
+            mapView.setFocusableInTouchMode(true);
+            mapView.requestFocus();
 
-            // 네이버 지도 표시
-            final Handler handler = new Handler();
-            new Thread() {
-                public void run() {
-                    try {
-                        String clientID = "a3h1sgm1T4A099FNlI1C";
-                        String clientSecret = "EUYqZGGwan";
-                        String addr = URLEncoder.encode(res_json.getString("location"), "UTF-8");
-                        String apiURL = "https://openapi.naver.com/v1/map/geocode?query=" + addr; //json
-                        //String apiURL = "https://openapi.naver.com/v1/map/geocode.xml?query=" + addr; // xml
-                        URL url = new URL(apiURL);
-                        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                        con.setRequestMethod("GET");
-                        con.setRequestProperty("X-Naver-Client-Id", clientID);
-                        con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-                        int responseCode = con.getResponseCode();
-                        BufferedReader br;
-                        if (responseCode == 200) { // 정상 호출
-                            br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                        } else {  // 에러 발생
-                            br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-                        }
-                        String inputLine;
-                        StringBuffer response = new StringBuffer();
-                        while ((inputLine = br.readLine()) != null) {
-                            response.append(inputLine);
-                        }
-                        br.close();
-
-                        JSONObject tempobj = new JSONObject(response.toString());
-                        String x_point = tempobj.getJSONObject("result").getJSONArray("items").getJSONObject(0).getJSONObject("point").getString("x");
-                        String y_point = tempobj.getJSONObject("result").getJSONArray("items").getJSONObject(0).getJSONObject("point").getString("y");
-
-                        apiURL = "https://openapi.naver.com/v1/map/staticmap.bin?clientId="
-                                + clientID + "&url=http://www.naver.com&center="
-                                + x_point + "," + y_point + "&level=12&w=1200&h=400&baselayer=default&markers="
-                                + x_point + "," + y_point;
-                        InputStream is = new URL(apiURL).openStream();
-                        final Bitmap bitmap = BitmapFactory.decodeStream(is);
-                        handler.post(new Runnable() {
-                            public void run() {
-                                ImageView img = (ImageView) findViewById(R.id.res_full_map);
-                                img.setImageBitmap(bitmap);
-                            }
-                        });
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
+            //disable coordinator layout when panning map
+            final NestedScrollView restaurant_content = (NestedScrollView) findViewById(R.id.nestedScrollView);
+            mapView.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    restaurant_content.requestDisallowInterceptTouchEvent(true);
+                    return false;
                 }
-            }.start();
+            });
 
+            // get langitude, latitude from address, change to NGeoPoint
+            ArrayList<Double> location = getLocation(res_json.getString("location"));
+            NGeoPoint point = new NGeoPoint(location.get(0), location.get(1));
+
+            mapView.setScalingFactor(1.6f);
+
+            NMapController mapController = mapView.getMapController();
+            if(mapController == null){
+                Log.e("mapController error", ">>setNaverMapview<<"+mapController);
+            }else{
+                mapController.setMapCenter(point, 13);
+            }
+
+            NMapViewerResourceProvider mMapViewerResourceProvider = new NMapViewerResourceProvider(this);
+            NMapOverlayManager mOverlayManager = new NMapOverlayManager(this, mapView, mMapViewerResourceProvider);
+
+            int markerId = NMapPOIflagType.PIN;
+
+            NMapPOIdata poiData = new NMapPOIdata(1, mMapViewerResourceProvider);
+            poiData.beginPOIdata(1);
+            poiData.addPOIitem(point, res_json.getString("name"), markerId, 0);
+            poiData.endPOIdata();
+
+            NMapPOIdataOverlay poiDataOverlay = mOverlayManager.createPOIdataOverlay(poiData, null);
+            poiDataOverlay.showFocusedItemOnly();
+
+            // when click 예약접수 (new Intent)
             TextView res_full_reserve = (TextView) findViewById(R.id.res_full_reserve);
             res_full_reserve.setOnClickListener(new View.OnClickListener(){
-
                 @Override
                 public void onClick(View v) {
                     Intent reservIntent = new Intent(getBaseContext(), Reservation.class);
@@ -276,6 +294,29 @@ public class FullRestaurant extends AppCompatActivity {
         }
 
     }
+
+    //주소를 받아서 위도와 경도를 return 해줌
+    private ArrayList<Double> getLocation(String address) {
+        Geocoder geocoder = new Geocoder(this);
+        Address addr;
+        ArrayList<Double> location = new ArrayList<Double>();
+        try {
+            List<Address> listAddress = geocoder.getFromLocationName(address, 1);
+            if (listAddress.size() > 0) { // 주소값이 존재 하면
+                addr = listAddress.get(0); // Address형태로
+                Double lat = (Double) (addr.getLatitude());
+                Double lng = (Double) (addr.getLongitude());
+                location.add(lng);
+                location.add(lat);
+
+                //Log.d(TAG, "주소로부터 취득한 경도 : " + location.get(0) + ", 위도 : " + location.get(1));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return location;
+    }
+
 
 }
 
